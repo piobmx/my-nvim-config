e14155c46b46cbe15f69105057bf5bf3606895e5
Zhangyi <glennwuu@outlook.com>
2023-08-28 23:24:27 +0200

    new nvim config

--- init.lua
+++ init.lua
@@ -39,5 +39,10 @@ require('usermod.barbar')
 --
 require('usermod.nvimtree')
 
-
 require("nvim-autopairs").setup {}
+
+
+--vim.api.nvim_set_hl(0, "TabLineFill", {fg="#12ffee"})
+vim.api.nvim_set_hl(0, "TabLineSel", {bold=true, fg="#1a2a3a", bg="#cecece"})
+vim.api.nvim_set_hl(0, "TabLine", {fg="#a4a4a4", bg="#4a4a4a"})
+vim.api.nvim_set_hl(0, "StatusLine", {fg="#cc1199", bg=nil})
--- lua/colrs.lua
+++ lua/colrs.lua
@@ -1,7 +1,22 @@
 -- https://www.ditig.com/256-colors-cheat-sheet
 -- https://github.com/termstandard/colors
 -- https://vimdoc.sourceforge.net/htmldoc/syntax.html
+-- https://neovim.io/doc/user/syntax.html#%3Ahighlight
 
+
+vim.api.nvim_set_hl(0, "Comment", { ctermfg=50, fg="#389a9c", italic=true })
+vim.api.nvim_set_hl(0, 'Normal', { ctermfg=113, fg = "#effeef" })
+-- 18ec47
+vim.api.nvim_set_hl(0, 'NonText', { ctermfg=123, fg = "#cacac1" })
+vim.api.nvim_set_hl(0, 'Visual', { fg='#87ff87', bg='#8787ff', ctermfg=120, ctermbg=105})
+vim.api.nvim_set_hl(0, 'Cursor', { ctermfg=120, fg="#1a3ac9", bg="#fff111" })
+vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#1e1e1e', fg = nil, underline = true })
+vim.api.nvim_set_hl(0, 'CursorColumn', {  fg="#fa1ac9", bg="#aeccff" })
+
+vim.api.nvim_set_hl(0, 'LineNrAbove', {  bg="#823494",  })
+
+-- hi Cursor     guifg=yellow guibg=gray  ctermfg=yellow ctermbg=gray
+-- hi lCursor    guifg=black  guibg=white ctermfg=black ctermbg=white
 
 -- highlight Comment ctermbg=DarkGray
 -- highlight Constant ctermbg=Blue
@@ -9,14 +24,4 @@
 -- highlight NonText ctermbg=Black
 -- highlight Special ctermbg=DarkMagenta
 -- highlight Cursor ctermbg=Green
-
-vim.api.nvim_set_hl(0, "Comment", { ctermfg=50, fg="#989a9c" })
--- vim.api.nvim_set_hl(0, "Comment", { ctermbg=DarkRed})
-vim.api.nvim_set_hl(0, 'Normal', { ctermfg=113, fg = "#68fc87" })
-vim.api.nvim_set_hl(0, 'NonText', { ctermfg=123, fg = "#cacac1" })
-vim.api.nvim_set_hl(0, 'Visual', { fg='#87ff87', bg='#8787ff', ctermfg=120, ctermbg=105})
-vim.api.nvim_set_hl(0, 'Cursor', { ctermfg=120, fg="#1a3ac9", bg="#fff111" })
-vim.api.nvim_set_hl(0, 'CursorColumn', {  fg="#fa1ac9", bg="#aeccff" })
--- hi Cursor     guifg=yellow guibg=gray  ctermfg=yellow ctermbg=gray
--- hi lCursor    guifg=black  guibg=white ctermfg=black ctermbg=white
 
